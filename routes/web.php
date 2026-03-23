baa8e9cc0955694069e037661d7be287b2e31315
partha201511 <parthabapon11@gmail.com>
2026-03-24 03:22:48 +0600

    Add VendorController and buy-stall route for Issue 12

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\VendorController;
 use App\Http\Controllers\Admin\FairController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +11,5 @@ Route::prefix('admin')->group(function () {
     Route::get('/fairs/create', [FairController::class, 'create'])->name('admin.fairs.create');
     Route::post('/fairs', [FairController::class, 'store'])->name('admin.fairs.store');
 });
+// Vendor Stall Kinar Route (Issue 12)
+Route::post('/buy-stall', [VendorController::class, 'buyStall']);
